3c0275764a4bb4c271a27f912ef6f4454601c255
Nelson Neto <nelson.ash@outlook.com>
2024-06-10 12:15:25 -0300

    teste de integração inicial

--- tests/Chronos.Tests/UnitTest1.cs
+++ tests/Chronos.Tests/UnitTest1.cs
@@ -1,10 +1,179 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+
 namespace Chronos.Tests;
 
-public class UnitTest1
+public class IntegrationTests : IClassFixture<ApplicationTestFixture>
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly ApplicationTestFixture _fixture;
+    private readonly HttpClient _client;
 
+    public IntegrationTests(ApplicationTestFixture fixture)
+    {
+        _fixture = fixture;
+        _client = fixture.CreateClient();
+    }
+
+    [Fact]
+    public async Task TestingGetRequest()
+    {
+        var result = await _client.GetAsync("WeatherForecast");
+
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task TestingGetRequest_WithSeparateBehavioursForEachClient()
+    {
+        var client = _fixture.WithWebHostBuilder(b =>
+        {
+            b.ConfigureTestServices(services =>
+            {
+                services.RemoveAll<IHttpClientFactory>();
+                services.AddSingleton<IHttpClientFactory, FakeHttpClientFactoryWhereTypedClientTwoWillFail>();
+            });
+        }).CreateClient();
+
+        var result = await client.GetAsync("WeatherForecast");
+
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
     }
 }
+public class ApplicationTestFixture : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing")
+                .ConfigureServices(services =>
+                {
+                    services.RemoveAll<IHttpClientFactory>();
+                    services.AddSingleton<IHttpClientFactory, FakeHttpClientFactory>();
+                });
+        return base.CreateHost(builder);
+    }
+}
+public class FakeHttpClientFactoryWhereTypedClientTwoWillFail : IHttpClientFactory
+{
+    public HttpClient CreateClient(string name)
+    {
+        return name switch
+        {
+            nameof(IMyTypedClient) => new HttpClient(new FakeHandler()),
+            _ => new HttpClient(new FakeHandler(new HttpResponseMessage(HttpStatusCode.BadRequest)))
+        };
+    }
+}
+public interface IMyTypedClient
+{
+    Task MakeRequest();
+}
+public class FakeHttpClientFactory : IHttpClientFactory
+{
+    public HttpClient CreateClient(string name)
+    {
+        return new HttpClient(new FakeHandler());
+    }
+}
+public class FakeHandler : DelegatingHandler
+{
+    private readonly HttpResponseMessage _response;
+
+    public FakeHandler(HttpResponseMessage? response = null)
+    {
+        _response = response ?? new HttpResponseMessage(HttpStatusCode.OK);
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_response);
+    }
+}
+/*
+internal sealed class XUnitLoggerProvider : ILoggerProvider
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly LoggerExternalScopeProvider _scopeProvider = new LoggerExternalScopeProvider();
+
+    public XUnitLoggerProvider(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new XUnitLogger(_testOutputHelper, _scopeProvider, categoryName);
+    }
+
+    public void Dispose()
+    {
+    }
+}
+internal sealed class XUnitLogger<T> : XUnitLogger, ILogger<T>
+{
+    public XUnitLogger(ITestOutputHelper testOutputHelper, LoggerExternalScopeProvider scopeProvider)
+            : base(testOutputHelper, scopeProvider, typeof(T).FullName)
+    {
+    }
+}
+
+internal class XUnitLogger : ILogger
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly string _categoryName;
+    private readonly LoggerExternalScopeProvider _scopeProvider;
+
+    public static ILogger CreateLogger(ITestOutputHelper testOutputHelper) => new XUnitLogger(testOutputHelper, new LoggerExternalScopeProvider(), "");
+    public static ILogger<T> CreateLogger<T>(ITestOutputHelper testOutputHelper) => new XUnitLogger<T>(testOutputHelper, new LoggerExternalScopeProvider());
+
+    public XUnitLogger(ITestOutputHelper testOutputHelper, LoggerExternalScopeProvider scopeProvider, string categoryName)
+    {
+        _testOutputHelper = testOutputHelper;
+        _scopeProvider = scopeProvider;
+        _categoryName = categoryName;
+    }
+
+    public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+
+    public IDisposable BeginScope<TState>(TState state) => _scopeProvider.Push(state);
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        var sb = new StringBuilder();
+        sb.Append(GetLogLevelString(logLevel))
+            .Append(" [").Append(_categoryName).Append("] ")
+            .Append(formatter(state, exception));
+
+        if (exception != null)
+        {
+            sb.Append('\n').Append(exception);
+        }
+
+        // Append scopes
+        _scopeProvider.ForEachScope((scope, state) =>
+        {
+            state.Append("\n => ");
+            state.Append(scope);
+        }, sb);
+
+        _testOutputHelper.WriteLine(sb.ToString());
+    }
+
+    private static string GetLogLevelString(LogLevel logLevel)
+    {
+        return logLevel switch
+        {
+            LogLevel.Trace => "trce",
+            LogLevel.Debug => "dbug",
+            LogLevel.Information => "info",
+            LogLevel.Warning => "warn",
+            LogLevel.Error => "fail",
+            LogLevel.Critical => "crit",
+            _ => throw new ArgumentOutOfRangeException(nameof(logLevel))
+        };
+    }
+}
+*/
